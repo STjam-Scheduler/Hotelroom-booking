37a09b8a2a0f2c5fcef232206a7f59017fb09a90
Maxim Becht <mabeit10@hs-esslingen.de>
2021-01-21 14:20:44 +0100

    Ez 10 sec bug fix

--- EasyHotelBooking24/Form1.h
+++ EasyHotelBooking24/Form1.h
@@ -1,5 +1,95 @@
 #pragma once
 
+public ref class element : public System::Windows::Forms::Form {
+private:
+	int y;
+public:
+
+	element(void)
+	{
+		InitializeComponent();
+		//
+		//TODO: Konstruktorcode hier hinzufügen.
+		//
+	}
+
+	element(int py) {
+		y = py;
+	}
+	~element()
+	{
+		if (components)
+		{
+			delete components;
+		}
+	}
+private: System::Windows::Forms::Button^ button;
+private: System::Windows::Forms::TextBox^ textBox1;
+
+protected:
+
+private:
+	/// <summary>
+	/// Erforderliche Designervariable.
+	/// </summary>
+	System::ComponentModel::Container^ components;
+
+#pragma region Windows Form Designer generated code
+	/// <summary>
+	/// Erforderliche Methode für die Designerunterstützung.
+	/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+	/// </summary>
+	void InitializeComponent(void)
+	{
+		this->button = (gcnew System::Windows::Forms::Button());
+		this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+		this->SuspendLayout();
+		// 
+		// button
+		// 
+		this->button->Font = (gcnew System::Drawing::Font(L"Times New Roman", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			static_cast<System::Byte>(0)));
+		this->button->Location = System::Drawing::Point(68, 90);
+		this->button->Name = L"button";
+		this->button->Size = System::Drawing::Size(487, 181);
+		this->button->TabIndex = 0;
+		this->button->Text = L"Room";
+		this->button->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+		this->button->UseVisualStyleBackColor = true;
+		this->button->Click += gcnew System::EventHandler(this, &element::room);
+		// 
+		// textBox1
+		// 
+		this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			static_cast<System::Byte>(0)));
+		this->textBox1->Location = System::Drawing::Point(322, 104);
+		this->textBox1->Multiline = true;
+		this->textBox1->Name = L"textBox1";
+		this->textBox1->Size = System::Drawing::Size(210, 150);
+		this->textBox1->TabIndex = 1;
+		this->textBox1->Text = L"\r\n";
+		// 
+		// Form1
+		// 
+		this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+		this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		this->ClientSize = System::Drawing::Size(804, 433);
+		this->Controls->Add(this->textBox1);
+		this->Controls->Add(this->button);
+		this->Name = L"Form1";
+		this->Text = L"Form1";
+		this->ResumeLayout(false);
+		this->PerformLayout();
+
+	}
+#pragma endregion
+private: System::Void room(System::Object^ sender, System::EventArgs^ e) {
+}
+
+
+};
+
+
 namespace Window {
 
 	using namespace System;
@@ -127,7 +217,7 @@ namespace Window {
 		}
 #pragma endregion
 	private: System::Void search_click(System::Object^ sender, System::EventArgs^ e) {
-		element^ elm =gcnew element();
+		element^ elm = gcnew element();
 	}
 private: System::Void listBox1_SelectedIndexChanged(System::Object^ sender, System::EventArgs^ e) {
 }
@@ -135,92 +225,3 @@ private: System::Void listBox1_SelectedIndexChanged(System::Object^ sender, Syst
 
 };
 }
-
-public ref class element : public System::Windows::Forms::Form {
-private: 
-	int y;
-public:
-
-	element(void)
-	{
-		InitializeComponent();
-		//
-		//TODO: Konstruktorcode hier hinzufügen.
-		//
-	}
-
-	element(int py) {
-		y = py;
-	}
-	~element()
-	{
-		if (components)
-		{
-			delete components;
-		}
-	}
-private: System::Windows::Forms::Button^ button;
-private: System::Windows::Forms::TextBox^ textBox1;
-
-protected:
-
-private:
-	/// <summary>
-	/// Erforderliche Designervariable.
-	/// </summary>
-	System::ComponentModel::Container^ components;
-
-#pragma region Windows Form Designer generated code
-	/// <summary>
-	/// Erforderliche Methode für die Designerunterstützung.
-	/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
-	/// </summary>
-	void InitializeComponent(void)
-	{
-		this->button = (gcnew System::Windows::Forms::Button());
-		this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-		this->SuspendLayout();
-		// 
-		// button
-		// 
-		this->button->Font = (gcnew System::Drawing::Font(L"Times New Roman", 10.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-			static_cast<System::Byte>(0)));
-		this->button->Location = System::Drawing::Point(68, 90);
-		this->button->Name = L"button";
-		this->button->Size = System::Drawing::Size(487, 181);
-		this->button->TabIndex = 0;
-		this->button->Text = L"Room";
-		this->button->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
-		this->button->UseVisualStyleBackColor = true;
-		this->button->Click += gcnew System::EventHandler(this, &element::room);
-		// 
-		// textBox1
-		// 
-		this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-			static_cast<System::Byte>(0)));
-		this->textBox1->Location = System::Drawing::Point(322, 104);
-		this->textBox1->Multiline = true;
-		this->textBox1->Name = L"textBox1";
-		this->textBox1->Size = System::Drawing::Size(210, 150);
-		this->textBox1->TabIndex = 1;
-		this->textBox1->Text = L"\r\n";
-		// 
-		// Form1
-		// 
-		this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
-		this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-		this->ClientSize = System::Drawing::Size(804, 433);
-		this->Controls->Add(this->textBox1);
-		this->Controls->Add(this->button);
-		this->Name = L"Form1";
-		this->Text = L"Form1";
-		this->ResumeLayout(false);
-		this->PerformLayout();
-
-	}
-#pragma endregion
-private: System::Void room(System::Object^ sender, System::EventArgs^ e) {
-}
-
-
-};
